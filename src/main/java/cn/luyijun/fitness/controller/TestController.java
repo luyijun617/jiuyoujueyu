6dc641de55bc387fdfbe2a5ea2a772801c968c2a
lyj <luyijun>
2019-09-06 17:46:04 +0800

    test

--- src/main/java/cn/luyijun/fitness/controller/TestController.java
+++ src/main/java/cn/luyijun/fitness/controller/TestController.java
@@ -1,20 +1,30 @@
 package cn.luyijun.fitness.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
 
-    private static int i = 100;
+    private  int i = 100;
     public static int j = 100;
-    private int a = 100;
-    public int b = 100;
+//    private int a = 100;
+//    public int b = 100;
 
-    public static void main(String[] args) {
+    @RequestMapping("/test")
+    @ResponseBody
+    public void test() {
         TestController testController = new TestController();
-        testController.i ++;
+        System.out.println(testController.i);
+        testController.i = 50;
+        System.out.println(testController.i);
         System.out.println(i);
-        testController = new TestController();
-        testController.i ++;
-        System.out.println(i);
-        TestController.i--;
-        System.out.println(i);
+        System.out.println(testController.i == i);
+//        testController = new TestController();
+//        testController.i ++;
+//        System.out.println(i);
+//        TestController.i--;
+//        System.out.println(i);
     }
 }
